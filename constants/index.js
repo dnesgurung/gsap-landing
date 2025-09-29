4cd77672cb46f060f8322914a9abea3daeb4f7be
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-09-29 13:48:52 +0100

    navbar section complete

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 export const navLinks = [
     { id: "cocktails",title: "Cocktails"},
     { id: "about",title: "About Us"},
-    { id: "about",title: "About Us"},
+    { id: "work",title: "The Art"},
+    { id: "contact",title: "Contact"},
 ]
